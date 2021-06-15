b8fb5af52d71a2cc45505d602073c5c9feca776f
gschiavon <germanschiavon@gmail.com>
2021-06-15 13:03:01 +0200

    update

--- src/main/scala/advanced/dynamic/DynamicPartitioning.scala
+++ src/main/scala/advanced/dynamic/DynamicPartitioning.scala
@@ -1,27 +1,10 @@
 package advanced.dynamic
 
-import org.apache.spark.sql.SparkSession
-
 object DynamicPartitioning extends App {
 
-  val spark: SparkSession = SparkSession
-    .builder()
-    .appName("DynamicInsert")
-    .master("local")
-    .getOrCreate()
-
-
-  val inputDF = spark.read.json("src/main/resources/datalake-input/")
-
-  inputDF
-    .coalesce(1)
-    .write
-    .partitionBy("date")
-    .mode("overwrite")
-    .json("src/main/resources/Datalake/")
 
 }
 
 
 
-//  spark.conf.set("spark.sql.sources.partitionOverwriteMode", "dynamic")
+
